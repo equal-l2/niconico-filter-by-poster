71af83a61c1d4209b7a628cccfab5994381505a9
equal-l2 <eng.equall2@gmail.com>
2021-12-08 18:55:50 +0900

    Comform the Standard

--- main.js
+++ main.js
@@ -1,130 +1,130 @@
 // Put all the javascript code here, that you want to execute after page load.
-let filters;
+let filters
 
 // filter all cards
-function filterAll() {
-  const cards = document.getElementsByClassName("NC-Card");
+function filterAll () {
+  const cards = document.getElementsByClassName('NC-Card')
   for (const c of cards) {
-    filterCard(c);
+    filterCard(c)
   }
 }
 
 // filter newly-added cards
-function run(muts) {
+function run (muts) {
   for (const m of muts) {
-    const nodes = m.addedNodes;
+    const nodes = m.addedNodes
     if (nodes.length > 0) {
-      filterCard(nodes[0]);
+      filterCard(nodes[0])
     }
   }
 }
 
 // find ids in filters
-function inFilters(ids) {
+function inFilters (ids) {
   for (const f of filters) {
     if (
       (ids.user !== undefined && ids.user === f.user) ||
       (ids.chan !== undefined && ids.chan === f.chan)
     ) {
-      return true;
+      return true
     }
   }
-  return false;
+  return false
 }
 
 // add filter effect to the specified card
-function filterCard(card) {
-  let id = getId(card);
-  if (id === "") return;
+function filterCard (card) {
+  const id = getId(card)
+  if (id === '') return
   getPoster(id).then((ids) => {
     if (inFilters(ids)) {
       // set title of card
       {
-        const title = card.getElementsByClassName("NC-CardTitle")[0];
-        if (title?.getElementsByClassName("NNFBP-Title").length === 0) {
-          const original = `${title.innerText}`;
-          title.innerText = "";
-          const span = document.createElement("span");
-          span.innerText = "Filtered";
-          span.title = original;
-          span.classList.add("NNFBP-Title");
-          title.appendChild(span);
+        const title = card.getElementsByClassName('NC-CardTitle')[0]
+        if (title?.getElementsByClassName('NNFBP-Title').length === 0) {
+          const original = `${title.innerText}`
+          title.innerText = ''
+          const span = document.createElement('span')
+          span.innerText = 'Filtered'
+          span.title = original
+          span.classList.add('NNFBP-Title')
+          title.appendChild(span)
         }
       }
 
       // add effect to thumbnail
       {
-        const thumb = card.getElementsByClassName("NC-Card-media")[0];
-        thumb?.classList.add("NNFBP-blur");
+        const thumb = card.getElementsByClassName('NC-Card-media')[0]
+        thumb?.classList.add('NNFBP-blur')
       }
     }
-  });
+  })
 }
 
 // get id of the card
-function getId(card) {
-  const link = card.getElementsByClassName("NC-Link")[0];
-  const matches = link.href.match(/(?:sm|so)\d*/);
+function getId (card) {
+  const link = card.getElementsByClassName('NC-Link')[0]
+  const matches = link.href.match(/(?:sm|so)\d*/)
   if (matches?.length === 1) {
-    return matches[0];
+    return matches[0]
   } else {
-    console.log("invalid uri:", link.href);
-    return "";
+    console.log('invalid uri:', link.href)
+    return ''
   }
 }
 
 // get poster of the movie
-async function getPoster(id) {
-  let uri = `https://ext.nicovideo.jp/api/getthumbinfo/${id}`;
+async function getPoster (id) {
+  const uri = `https://ext.nicovideo.jp/api/getthumbinfo/${id}`
   try {
-    let res = await fetch(uri);
-    let text = await res.text();
-    let xml = new window.DOMParser().parseFromString(text, "text/xml");
-    let userId = xml.getElementsByTagName("user_id")[0]?.textContent;
-    let chanId = xml.getElementsByTagName("ch_id")[0]?.textContent;
+    const res = await fetch(uri)
+    const text = await res.text()
+    const xml = new window.DOMParser().parseFromString(text, 'text/xml')
+    const userId = xml.getElementsByTagName('user_id')[0]?.textContent
+    const chanId = xml.getElementsByTagName('ch_id')[0]?.textContent
     return {
       user: userId,
-      chan: chanId,
-    };
+      chan: chanId
+    }
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
 }
 
 browser.storage.sync
-  .get("filters")
+  .get('filters')
   .then((res) => {
-    console.log("NNFBP filters loaded");
-    filters = res.filters;
+    console.log('NNFBP filters loaded')
+    filters = res.filters
     if (filters === undefined) {
-      filters = [];
+      filters = []
     }
 
-    const observer = new MutationObserver(run);
+    const observer = new MutationObserver(run)
 
-    const rows = document.getElementsByClassName("RankingMatrixVideosRow");
+    const rows = document.getElementsByClassName('RankingMatrixVideosRow')
     for (const row of rows) {
-      observer.observe(row, { childList: true });
+      observer.observe(row, { childList: true })
     }
 
     // remove ads
-    const adRows = document.getElementsByClassName("RankingMatrixNicoadsRow");
+    const adRows = document.getElementsByClassName('RankingMatrixNicoadsRow')
     for (const row of adRows) {
-      row.remove();
+      row.remove()
     }
 
-    filterAll();
+    filterAll()
 
     browser.storage.onChanged.addListener((changes, area) => {
-      if (area === "sync" && changes.filters?.newValue !== undefined) {
-        filters = changes.filters.newValue;
-        console.log("NNFBP filters reloaded");
-        filterAll();
+      if (area === 'sync' && changes.filters?.newValue !== undefined) {
+        filters = changes.filters.newValue
+        console.log('NNFBP filters reloaded')
+        filterAll()
       }
-    });
+    })
 
-    console.log("NNFBP enabled");
+    console.log('NNFBP enabled')
   })
   .catch((e) => {
-    console.log("NNFBP failed to load filter: ", e);
-  });
+    console.log('NNFBP failed to load filter: ', e)
+  })
--- options.js
+++ options.js
@@ -1,85 +1,93 @@
-// generate filter for the uri specified via form
-function generateFilter() {
-  const mesg = document.getElementById("gen-mesg");
-  const form = document.getElementById("uri");
-  const link = form.value;
-  import("./utils.js").then(utils => {
+let loaded = false
+document.addEventListener('DOMContentLoaded', () => { loaded = true })
+
+import('./utils.js').then(utils => {
+  // generate filter for the uri specified via form
+  function generateFilter () {
+    const mesg = document.getElementById('gen-mesg')
+    const form = document.getElementById('uri')
+    const link = form.value
     utils.generateFilterFromURL(link).then(([vid, filter, err]) => {
       if (err === null) {
-        document.getElementById("filters").value += "\n" + formatFilter(filter);
-        mesg.innerText = `Generated for ${vid}`;
+        document.getElementById('filters').value += '\n' + formatFilter(filter)
+        mesg.innerText = `Generated for ${vid}`
       } else {
-        mesg.innerText = err;
+        mesg.innerText = err
       }
     })
-  })
-}
-
-// format filter object to string
-function formatFilter(filt) {
-  if (filt.user) {
-    return `user:${filt.user}`;
-  } else if (filt.chan) {
-    return `chan:${filt.chan}`;
-  } else {
-    return "";
   }
-}
 
-// parse form string into array of filter object
-function parseFilters(str) {
-  const lines = str.split("\n");
-  let res = {
-    filters: [],
-    errorLine: 0,
-  };
-  for (const [i, l] of lines.entries()) {
-    const s = l.trim();
-    if (s.length === 0) continue;
-
-    const entry = s.match(/(user|chan):(\d+)/);
-    if (entry?.length === 3) {
-      const userId = entry[1] === "user" ? entry[2] : "";
-      const chanId = entry[1] === "chan" ? entry[2] : "";
-      res.filters.push({
-        user: userId,
-        chan: chanId,
-      });
+  // format filter object to string
+  function formatFilter (filt) {
+    if (filt.user) {
+      return `user:${filt.user}`
+    } else if (filt.chan) {
+      return `chan:${filt.chan}`
     } else {
-      res.errorLine = i + 1;
-      break;
+      return ''
     }
   }
 
-  res.filters = uniqFilters(res.filters);
-  return res;
-}
+  // parse form string into array of filter object
+  function parseFilters (str) {
+    const lines = str.split('\n')
+    const res = {
+      filters: [],
+      errorLine: 0
+    }
+    for (const [i, l] of lines.entries()) {
+      const s = l.trim()
+      if (s.length === 0) continue
 
-function saveOptions() {
-  const text = document.getElementById("filters").value;
-  const parsed = parseFilters(text);
-  if (parsed.errorLine === 0) {
-    browser.storage.sync.set({
-      filters: parsed.filters,
-    });
-    document.getElementById("mesg").innerText = "Saved!";
+      const entry = s.match(/(user|chan):(\d+)/)
+      if (entry?.length === 3) {
+        const userId = entry[1] === 'user' ? entry[2] : ''
+        const chanId = entry[1] === 'chan' ? entry[2] : ''
+        res.filters.push({
+          user: userId,
+          chan: chanId
+        })
+      } else {
+        res.errorLine = i + 1
+        break
+      }
+    }
+
+    res.filters = utils.uniqFilters(res.filters)
+    return res
+  }
+
+  function saveOptions () {
+    const text = document.getElementById('filters').value
+    const parsed = parseFilters(text)
+    if (parsed.errorLine === 0) {
+      browser.storage.sync.set({
+        filters: parsed.filters
+      })
+      document.getElementById('mesg').innerText = 'Saved!'
+      restoreOptions()
+    } else {
+      document.getElementById(
+        'mesg'
+      ).innerText = `Error on line ${parsed.errorLine}`
+    }
+  }
+
+  function restoreOptions () {
+    browser.storage.sync.get('filters').then((res) => {
+      if (res.filters) {
+        const str = res.filters.map((f) => formatFilter(f)).join('\n')
+        document.getElementById('filters').value = str
+      }
+    })
+  }
+
+  if (loaded) {
+    restoreOptions()
   } else {
-    document.getElementById(
-      "mesg"
-    ).innerText = `Error on line ${parsed.errorLine}`;
+    document.addEventListener('DOMContentLoaded', restoreOptions)
   }
-  restoreOptions();
-}
 
-function restoreOptions() {
-  browser.storage.sync.get("filters").then((res) => {
-    if (res.filters) {
-      const str = res.filters.map((f) => formatFilter(f)).join("\n");
-      document.getElementById("filters").value = str;
-    }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.getElementById("apply").addEventListener("click", saveOptions);
-document.getElementById("generate").addEventListener("click", generateFilter);
+  document.getElementById('apply').addEventListener('click', saveOptions)
+  document.getElementById('generate').addEventListener('click', generateFilter)
+})
